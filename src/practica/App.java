e6b358f8ec9faa0dbb4b98a52936041ec74a184b
Raynou <rafaelgame2012@gmail.com>
2021-05-20 13:59:34 -0500

    das

--- src/practica/App.java
+++ src/practica/App.java
@@ -5,17 +5,14 @@ public class App {
 
         //Al momento de crear el objeto se pasa como parámetro el área de la base y luego la altura del cilindro
 
-        //Se puede crear un objeto de tipo círculo
         Circulo miCirculo = new Circulo(6.2f);
     
         Cilindro miCilindro = new Cilindro(miCirculo, 5.7f); 
 
-        //Y pasarlo como parámetro en el contructor del cilindro
         Cilindro miOtroCilindro = new Cilindro(miCirculo, 8.9f);
         
         DecimalFormat df = new DecimalFormat("#.00");  
         
-        /* Se desde la clase Cilindro se llaman a los métodos heredados de la clase Circulo */
         
         /* Primer cilindro */
         
